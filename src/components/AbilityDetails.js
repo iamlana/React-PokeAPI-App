a8f55e88a273db92652870dd5be61afb42fb3427
Anna Lukianova <ann.lookyanova@gmail.com>
2020-04-15 17:32:04 +0200

    Fix errors in AbilityDetails

--- src/components/AbilityDetails.js
+++ src/components/AbilityDetails.js
@@ -16,20 +16,18 @@ export function AbilityDetails({ match }) {
     <>
       <div className="page">
         <Link to={'/ability'}><button >&#8592;All Abilities</button></Link>
-        <h1 key={ability.name}>{ability.name}</h1>
-        <p>{ability.effect_entries.map(changes => (
-          <> {changes.effect}</>
+        <h1>{ability.name}</h1>
+        <p>{ability.effect_entries.map((changes, i) => (
+          <React.Fragment key={i}>{changes.effect}</React.Fragment>
         ))}
         </p>
       </div>
       <p>Pokémons, who have this Ability:</p>
-      <p className="grid-container" key={ability.name}>{ability.pokemon.map(pokemon => (
-        <>
-          <PokemonLink key={pokemon.name} id={pokemon.pokemon.name} />
-          <br />
-        </>
-      ))}
-      </p>
+      <div className="grid-container">
+        {ability.pokemon.map(pokemon => (
+          <PokemonLink key={pokemon.pokemon.name} id={pokemon.pokemon.name} />
+        ))}
+      </div>
     </>
   )
 }
