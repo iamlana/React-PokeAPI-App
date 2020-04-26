43a84839b3cda916feae9473a9685cbf76da6250
Anna Lukianova <ann.lookyanova@gmail.com>
2020-04-26 20:29:38 +0200

    Create color changing using mouse hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,29 @@
 import React from "react"
+import { useMousePosition } from "../hooks/useMousePosition"
 
 export function Home() {
+
+  const { x, y } = useMousePosition();
+  const hasMovedCursor = typeof x === "number" && typeof y === "number";
+  const colorX = "#" + `${x}`
+  const colorY = "#" + `${y}`
+function backGroundVGrad(){
+  document.body.style.backgroundImage = "linear-gradient(" + colorX + "," + colorY + ")"
+}//style={{ backgroundImage: "linear-gradient(" + colorX + "," + colorY + ")" }}
+
+
+// {hasMovedCursor
+//   ? `Your cursor is at ${x}, ${y}.`
+//   : "Move your mouse around."}
   return (
     <div className="page">
       <h1>Home</h1>
       <p>Open the sidebar to view all the links!</p>
+      <p>
+        {hasMovedCursor
+          ? backGroundVGrad()
+          : "Move your mouse around."}
+      </p>
     </div>
   )
 }
